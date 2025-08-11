35181db60676b19aa971fa062512fb7928640089
krishna soni <110759309+krishnaSONI011@users.noreply.github.com>
2025-08-12 00:08:42 +0530

    aos update

--- src/app/aos.tsx
+++ src/app/aos.tsx
@@ -1,12 +1,12 @@
 // app/aos.ts
 "use client"
-import { useEffect } from "react";
-import AOS from "aos";
+import { useEffect } from "react"
+import Aos from "aos";
 import "aos/dist/aos.css";
 
 export default function AosInit() {
   useEffect(() => {
-    AOS.init({
+    Aos.init({
       duration: 800,
       once: true,
     });
